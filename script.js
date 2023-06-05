d12d906975944afd0bafc4f7c789a770f89e4ae9
dothereth <135543272+dothereth@users.noreply.github.com>
2023-06-05 19:07:46 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-document.getElementById("mintButton").addEventListener("click", function() {
-    alert("OK");
+const provider = new WalletConnectProvider({
+  rpc: {
+    1: 'https://mainnet.infura.io/v3/23505897e91d4f858f48eb1a4501904d',
+  },
+});
+
+document.getElementById('connectButton').addEventListener('click', () => {
+  provider.enable().then(() => {
+    const web3 = new Web3(provider);
+    web3.eth.getAccounts().then(accounts => {
+      const userAddress = accounts[0];
+      // Perform operations with the connected wallet
+    });
+  });
 });
